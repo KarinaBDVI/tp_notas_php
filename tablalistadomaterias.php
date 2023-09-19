7a3917deb109bd2a280b7c5cd49e2becc3642aa4
@KarinaDVI <kbbouza@gmail.com>
2023-09-19 03:58:06 -0300

    listado materias

--- tablalistadomaterias.php
+++ tablalistadomaterias.php
@@ -6,6 +6,8 @@
     <title>Home-ISFT 225</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
+
     <link rel="stylesheet" href="./styles/style.css">
     <link rel="stylesheet" href="./styles/styletablas.css">
 </head>
@@ -18,9 +20,6 @@
             <div class="d-block p-3 m-4 h-100 ">
                 <h3 class="card-footer-text mt-2 mb-5 p-2">Materias</h3>
              
-                
-                        
-                       
     <!-- Contenedor principal -->
     <div class="d-flex flex-nowrap sidebar-height"> 
       <!-- Aside/Wardrobe/Sidebar Menu --> 
@@ -41,7 +40,7 @@
                     <th>Carrera</th>
                     <th>Materia</th>
                     <th>Tipo Aprobación</th>
-                    <th>Código Númerico</th>
+                    <th>Código Numérico</th>
                     <th>Código Alfabético</th>
                     <th>Estado</th>
                     <th>Editar</th>
@@ -63,8 +62,8 @@
                     foreach ($fila as $valor) {
                         echo "<td>$valor</td>";
                     }
-                    echo "<td><a href='#' class='btn btn-warning'><i class='fas fa-pencil-alt'></i></a></td>";
-                    echo "<td><a href='#' class='btn btn-info'><i class='fas fa-eye'></i></a></td>";
+                    echo "<td><a href='#' class='btn btn-custom-edit'><i class='fas fa-pencil-alt'></i></a></td>";
+                    echo "<td><a href='#' class='btn btn-custom-view'><i class='fas fa-eye'></i></a></td>";
                         ;
 
 
